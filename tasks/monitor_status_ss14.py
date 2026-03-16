879e7ea0da11f51bc5319466801432a2ef53778f
Schrödinger <132720404+Schrodinger71@users.noreply.github.com>
2026-03-16 20:27:33 +0300

    - fix monitor_status_ss14

--- tasks/monitor_status_ss14.py
+++ tasks/monitor_status_ss14.py
@@ -10,7 +10,6 @@ async def monitor_status_ss14():
     """
     Фоновая задача для мониторинга статуса сервера SS14.
     """
-    # Здесь будет код для получения статуса сервера и отправки уведомлений при изменении
     log.info("🔍 Проверка статуса сервера SS14...")
 
     url = f"http://{env_cfg.IP_HOST}:1212/status"
@@ -21,7 +20,9 @@ async def monitor_status_ss14():
                 if resp.status == 200:
                     data = await resp.json()
                     log.info(f"✅ Статус сервера: {data['players']}/{data['soft_max_players']} игроков на карте {data['map']}")
-                    bot.change_presence(activity=Game(name=f"{data['players']}/{data['soft_max_players']} на {data['map']}"))
+                    await bot.change_presence(
+                        activity=Game(name=f"{data['players']}/{data['soft_max_players']} на {data['map']}")
+                    )
                 else:
                     log.warning(f"⚠️ Не удалось получить статус сервера: код {resp.status}")
     except Exception as e:
