d6418d78c7df1cd45b831fbc7eaf68af86c1a2a4
Schrödinger <132720404+Schrodinger71@users.noreply.github.com>
2026-03-19 23:58:57 +0300

    Test

--- commands/misc/general_commands.py
+++ commands/misc/general_commands.py
@@ -11,7 +11,7 @@ async def ping(ctx):
 
     latency = round(bot.latency * 1000)
     emoji = "🏓" if latency < 100 else "🐢"
-    await ctx.send(f"{emoji} Pong!!! Задержка: **{latency}ms**")
+    await ctx.send(f"{emoji} Pong! Задержка: **{latency}ms**")
 
 
 @bot.slash_command(name="ping", help="Проверяет задержку бота.")
@@ -22,4 +22,4 @@ async def ping_command(interaction: AppCommandInteraction):
 
     latency = round(bot.latency * 1000)
     emoji = "🏓" if latency < 100 else "🐢"
-    await interaction.response.send_message(f"{emoji} Pong!!! Задержка: **{latency}ms**")
+    await interaction.response.send_message(f"{emoji} Pong! Задержка: **{latency}ms**")
